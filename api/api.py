138288a536ebde8c695a1a2ef51428208fdaa998
agnij-dutta <duttaagnij01@gmail.com>
2024-11-17 04:00:50 +0530

    final changes

--- api/api.py
+++ api/api.py
@@ -8,7 +8,10 @@ app = FastAPI()
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://hacktivate.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
